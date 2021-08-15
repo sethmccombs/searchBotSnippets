5dcdf242a3d7c44c5dcb5f30b37096adafd46794
Seth McCombs <seth.mccombs@workday.com>
2021-08-14 20:26:46 -0700

    requirements

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
+from flask import Flask
 from serpapi import GoogleSearch
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+
+@app.route('/')
+
+def hello_world():
+   return('Hello World')
 
 def search_google(query_term):
 
@@ -18,7 +27,7 @@ def search_google(query_term):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-
+    app.run(debug = True)
     print("What should I search for?")
-    search_term = input("Search Term: ")
-    print(search_google(search_term))
+    # search_term = input("Search Term: ")
+    # print(search_google(search_term))
