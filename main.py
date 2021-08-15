de20fd64685c74ecc236ea3accda288919852898
Seth McCombs <seth.mccombs@workday.com>
2021-08-14 20:33:56 -0700

    updates to flask

--- main.py
+++ main.py
@@ -1,10 +1,38 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from serpapi import GoogleSearch
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
 
+
+@app.route('/getmsg/', methods=['GET'])
+def respond():
+    # Retrieve the name from url parameter
+    name = request.args.get("name", None)
+
+    # For debugging
+    print(f"got name {name}")
+
+    response = {}
+
+    # Check if user sent a name at all
+    if not name:
+        response["ERROR"] = "no name found, please send a name."
+    # Check if the user entered a number not a name
+    elif str(name).isdigit():
+        response["ERROR"] = "name can't be numeric."
+    # Now the user entered a valid name
+    else:
+        response["MESSAGE"] = f"Welcome {name} to our awesome platform!!"
+
+    # Return the response in json format
+    return jsonify(response)
+
+
 @app.route('/')
+def index():
+    return "<h1>Welcome to our server !!</h1>"
+
 
 def hello_world():
    return('Hello World')
@@ -27,7 +55,8 @@ def search_google(query_term):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run()
+    # app.run(debug = True)
     print("What should I search for?")
     # search_term = input("Search Term: ")
     # print(search_google(search_term))
